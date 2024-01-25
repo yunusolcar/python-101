397ca88aabfbdcc34579881c94207ebdc554eb12
yunus <yunusolcar@hotmail.com>
2024-01-25 20:42:10 +0300

    python - zero to hero

---
+++ index.py
@@ -0,0 +1,38 @@
+x = "selam"
+print(x + " sd4")
+
+# Data Objects:
+# 1- Scalar
+# 2 - Non-scalar
+
+
+# Data Types
+# Integer
+# 1 - 2  - 3 - 4 - 5
+
+# Float
+# 2.5 - 10.2   Not: 2.0 da floattır.
+
+# Boolean
+# True - false
+
+
+# type
+# type(2.3) yazarak hangi type  da olduğunu öğrenebiliriz
+
+# type casting
+# float olan sayıyı integere çevirme
+# int(2.8) -> çıktı -> 2 olarak integer e dönüştü
+
+
+# Variable Assingment
+# a = 2
+# x = a + 5    => x = 7
+# a = a + 3    => a = 5
+
+# Non-Scaler
+# String
+# "ATATURK"
+# type("1")    => str
+
+
